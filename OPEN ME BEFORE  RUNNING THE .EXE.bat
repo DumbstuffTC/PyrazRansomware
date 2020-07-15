e271e2b837c7315cfe1ed58dca9e478c3b37caff
TrueCubeYT <61812799+TrueCubeYT@users.noreply.github.com>
2020-07-15 12:00:24 -0400

    Create OPEN ME BEFORE  RUNNING THE .EXE.bat

---
+++ OPEN ME BEFORE  RUNNING THE .EXE.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Hello there, you probably actually listened! The .exe file is a ransomware that encrypts all your files in the homepatg  to a random file > README.txt
+echo. >> README.txt
+echo Please note that there are no decryptors for this ransomware, it has a fake decryptor, you need to decrypt the files yourself by changing the file extensions >> README.txt
+echo If you do not want to potentially I do not recommend running this on your main PC >> README.txt
+echo. >> README.txt
+echo Other than those warnings, this ransomware is just a joke, it is as simple as changing the file extensions to get your files back!! >> README.txt
+start README.txt
